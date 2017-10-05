00cd3a7494a2e9b54b962955875005637f9becd3
Blaine Richardson <blainerrichardson@gmail.com>
2017-10-05 17:47:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,3 +6,15 @@ class App < Sinatra::Base
     enable :sessions
     set :session_secret, "flatiron"
   end
+
+  get "/" do
+    erb :index
+  end
+
+  post "checkout" do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
+  
+end
